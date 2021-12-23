f87a1f6e092a864cd9279e82b5fa589f0b6d01bd
Michael Thierry <michaelthierry86@gmail.com>
2021-12-23 14:11:19 -0400

    Adicionado os arquivos

---
+++ Listas/Lista_estatica/lista.h
@@ -0,0 +1,9 @@
+/** Estrutura: Lista Estattica Linear
+ *  Modulo: Incrementado
+ *  Autor: Michael Thierry 
+*/
+#ifndef LISTA_H
+#define LISTA_H
+#define MAX 32
+//Defindo um tipo para a lista
+#endif
